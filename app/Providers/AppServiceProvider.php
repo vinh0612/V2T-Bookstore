9ac7633cf1c42084f62e0d406f46f344f2115724
Who Think <huuthinh31082000@gmail.com>
2026-06-15 11:29:01 +0700

    Sửa lại URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 use App\Models\Category;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // <-- THÊM ĐOẠN NÀY: Ép Laravel dùng link HTTPS khi chạy trên mạng (Render)
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         // Chia sẻ biến $categories cho tất cả các file trong folder partials
         View::composer('partials.header', function ($view) {
             $megamenuCategories = Category::whereNull('parent_id') // Lấy danh mục gốc (Cha lớn)
